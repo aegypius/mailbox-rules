8485e481b39664a834a0d2c4fa4cfc6844c6210f
aegypius <git@aegypius.com>
2026-03-01 20:54:55 +0100

    chore: update configuration to fix my rules too

--- ecs.php
+++ ecs.php
@@ -9,6 +9,8 @@ return function (ECSConfig $ecsConfig): void {
     $ecsConfig->paths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
+        __DIR__ . '/my-rules.php',
+        __DIR__ . '/rules.php',
         __FILE__,
 
         # Add dagger module too
